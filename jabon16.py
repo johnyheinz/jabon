c70e4c3924ea60b1d1af75950295f4147349b8f5
johnyheinz <heinzjohny@gmail.com>
2022-08-24 21:13:46 +0300

    0.1.5
    - added stone/scissors/paper

--- jabon16.py
+++ jabon16.py
@@ -2,12 +2,20 @@ import os
 import random
 from vkbottle.bot import Bot, Message
 from vkbottle.dispatch.rules.base import CommandRule
+from vkbottle import BaseMiddleware
+import game
 
 bot = Bot(os.environ['API_KEY'])
 
+class LogMiddleware(BaseMiddleware[Message]):
+    async def post(self):
+        if not self.handlers:
+            return
+        print("\n"+f"{self.handle_responses}"+"\n")
+
 @bot.on.message(text=['!help','!cmd']) #вызов списка доступных команд
 async def command_list(message: Message):
-    await message.answer("Список команд:\n !roll [от a до b] \n !echo [дублирует написанную фразу]")
+    await message.answer("Список команд:\n !roll [от a до b] \n !echo [дублирует написанную фразу] \n !ssp [оружие: камень, ножницы, бумага|сложность: нормально, невозможно]")
 
 @bot.on.message(CommandRule("roll",["!"],2,sep=" ")) #алгоритмы у вольво (теперь настраивается лол)
 async def roller(message: Message):
@@ -23,9 +31,17 @@ async def roller(message: Message):
 async def echo_answer(message: Message):
     await message.answer(message.text[6:])
 
-@bot.on.message() #что-то типа логов
-async def logwrite(message: Message):
+@bot.on.message(CommandRule("ssp",["!"],2,sep=" ")) #первая типа игра или что-то такое, скорее тест подключаемых модулей
+async def sspgame(message: Message):
     users_info = await bot.api.users.get(message.from_id)
-    await message.answer("{}".format(users_info[0].first_name)+" исполнил: "+message.text)    
+    sspstring = message.text[5:]
+    sspstring = sspstring.strip()
+    sspstring = sspstring.split()
+    sspweapon = sspstring[0]
+    sspdiffculty = sspstring[1]
+    print(f"\n {sspweapon} \n")
+    print(f"\n {sspdiffculty} \n")
+    await message.answer(game.ssp(sspweapon,sspdiffculty))
 
+bot.labeler.message_view.register_middleware(LogMiddleware)
 bot.run_forever()
